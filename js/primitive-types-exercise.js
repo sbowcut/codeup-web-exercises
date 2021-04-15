2748c0aa0cb86ce9c8ebecdeb88097c5243b7252
Sam Bowcut <sbowcut@gmail.com>
2021-04-15 15:15:30 -0600

    Commit 4-15-21

---
+++ js/primitive-types-exercise.js
@@ -0,0 +1,10 @@
+"use strict"
+
+console.log(typeof 99.9);
+console.log(typeof "false");
+console.log(typeof false);
+console.log(typeof '0');
+console.log(typeof 0);
+console.log(typeof 'true');
+console.log(typeof true);
+
---
+++ js/shorthand-unary-and-jsmethods.js
@@ -0,0 +1,33 @@
+"use strict";
+
+//SHORTHAND ASSIGNMENT OPERATORS
+
+// += / -=
+
+// var name = "Sam"
+//
+// name += " Bowcut"
+// console.log(name)
+
+// -= is only for numbers
+
+// var daysRemainingInCohort = 114;
+//
+// daysRemainingInCohort -= 4;
+//
+// console.log(daysRemainingInCohort);
+
+var qu1 = 2
+console.log(qu1 += 2);
+
+var qu2 = 10
+console.log(qu2 -= 6);
+
+var qu3 = 4
+console.log(qu3 *= 1);
+
+var qu4 = 12
+console.log(qu4 /= 3);
+
+var qu5 = 19
+console.log(qu5 %= 5);
