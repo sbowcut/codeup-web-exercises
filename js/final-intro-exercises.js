f9af604c063b89ffcd940009d372debcbd7c1b70
Sam Bowcut <sbowcut@gmail.com>
2021-04-15 20:39:44 -0600

    4-15-21

---
+++ js/final-intro-exercises.js
@@ -0,0 +1,100 @@
+"use strict"
+
+// var a = 1;
+// var b = a++;
+// var c = ++a;
+//
+// console.log(a);
+
+// var d = "hello";
+// var e = false;
+//
+// console.log(d++);
+// console.log(e++);
+
+// Why is false++ = 0?!!!!!
+
+// var perplexed; // perplexed is undefined (no value is assigned)
+// perplexed + 2;
+
+// console.log(perplexed + 2); NaN
+
+// var price = 2.7;
+// price = price.toFixed(2);
+// console.log(price);
+
+// var price = "2.7";
+// price = price.toFixed(2);
+// console.log(price);
+
+// console.log(isNaN(0)); //false
+//
+// console.log(isNaN(1)); //false
+//
+// console.log(isNaN("")); //false!!!!!!!!!
+//
+// console.log(isNaN("string")); //true
+//
+// console.log(isNaN("0")); //false
+//
+// console.log(isNaN("1")); //false
+//
+// console.log(isNaN("3.145")); //false
+//
+// console.log(isNaN(Number.MAX_VALUE)); //false
+//
+// console.log(isNaN(Infinity)); //false
+//
+// console.log(isNaN("true")); //true
+//
+// console.log(isNaN(true)); //false
+//
+// console.log(isNaN("false")); //true
+//
+// console.log(isNaN(false)); //false
+
+// console.log(!true); //false
+//
+// console.log(!false); //true
+//
+// console.log(!!true); //true
+//
+// console.log(!!false); //false
+//
+// console.log(!!0); //false
+//
+// console.log(!!-0); //false
+//
+// console.log(!!1); //false
+//
+// console.log(!!-1); //true!!!!!!!!!!
+//
+// console.log(!!0.1); //true
+//
+// console.log(!!"hello"); //true
+//
+// console.log(!!""); //false
+//
+// console.log(!!''); //false
+//
+// console.log(!!"false"); //true
+//
+// console.log(!!"0"); //true
+
+// var sample = "Hello Codeup";
+// console.log(sample.length);
+// sample = sample.toUpperCase();
+// sample = sample.toLowerCase();
+// sample = (sample + " Students");
+// var str1 = " students";
+// var mix = sample.concat(str1);
+// console.log(mix);
+
+// console.log(sample.indexOf("c")); // -1?!!!!!!!!
+
+// var sample2 = sample.substr(6,12);
+// console.log(sample2);
+
+var username = 'codeup';
+var password = 'notastrongpassword';
+
--- js/shorthand-unary-and-jsmethods.js
+++ js/shorthand-unary-and-jsmethods.js
@@ -17,17 +17,72 @@
 //
 // console.log(daysRemainingInCohort);
 
-var qu1 = 2
-console.log(qu1 += 2);
+// var qu1 = 2
+// console.log(qu1 += 2);
+//
+// var qu2 = 10
+// console.log(qu2 -= 6);
+//
+// var qu3 = 4
+// console.log(qu3 *= 1);
+//
+// var qu4 = 12
+// console.log(qu4 /= 3);
+//
+// var qu5 = 19
+// console.log(qu5 %= 5);
 
-var qu2 = 10
-console.log(qu2 -= 6);
+//Unary
 
-var qu3 = 4
-console.log(qu3 *= 1);
+// var makeItANumber = "5";
+// console.log(+makeItANumber);
 
-var qu4 = 12
-console.log(qu4 /= 3);
+//INCREMENT/DECREMENT
 
-var qu5 = 19
-console.log(qu5 %= 5);
+//post-increment (num++)
+
+// var num = 10;
+
+// console.log(num++); //num += 1
+// console.log(num);
+//
+// var num1 = 20;
+//
+// console.log(num1--); //num -= 1
+// console.log(num1);
+
+// JS built-in methods
+
+// toString();
+
+// var num = 5 * 5;
+// //to convert
+// num = num.toString();
+//
+// console.log(num.toString());
+// console.log(typeof num);
+
+// var pi = 3.1415926535;
+//
+// console.log(pi.toFixed(2));
+
+//Number() =- checks to see if the value is a Number
+
+// console.log(Number("Sam"));
+
+// isNaN() - checks to see if the value provides is Not-a-Number
+
+// console.log(isNaN("Sam"));
+
+// parseInt() Truncates the number at the start of a string
+
+// console.log(parseInt("12122212121 what a number!"));
+
+// .length - Counts the characters in a string.
+
+// console.log("Sam".length);
+
+// .indexOf - get index # of character in string (starts counting at 0)
+
+// console.log("Sam".indexOf("a"));
+
