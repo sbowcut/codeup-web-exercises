33a3b3892629380b1bba4b18421c607de96c60e2
Sam Bowcut <sbowcut@gmail.com>
2021-04-20 10:25:27 -0600

    4-20-21

--- js/conditionals-lecture.js
+++ js/conditionals-lecture.js
@@ -1,13 +1,33 @@
 (function(){
     "use strict";
 
-    var hasCar;
+    // var hasCar;
+    //
+    // if (hasCar){
+    //     console.log("Let's get in and go!")
+    // }else{
+    //     console.log("We ned to call Uber")
+    // }
+    function getToDestination(age, isInsured, hasCar){
+        var isLicensed = age >= 16;
+        var isLegalDriver = isLicensed && isInsured;
 
-    if (hasCar){
-        console.log("Let's get in and go!")
-    }else{
-        console.log("We ned to call Uber")
+        if (isLegalDriver && hasCar){
+            console.log("Let's get in and go!");
+        }else if ((!isLegalDriver || !hasCar) && (canGetRideShare())){
+            console.log("We need to call Uber!");
+        }else{
+            console.log("I think my friend can give us a ride")
+        }
     }
+        getToDestination(16, false, true);
+        canGetRideShare(5,30)
+    function canGetRideShare(nearbyDrivers, hasEnoughMoney){
+        var enoughMoney = hasEnoughMoney;
+        var driverClose = nearbyDrivers;
+            if (enoughMoney){
+            }
+
 
 
 
