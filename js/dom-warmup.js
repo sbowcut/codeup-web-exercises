26801f60e07d3e4fb7789ce5a577af9829203817
Sam Bowcut <sbowcut@gmail.com>
2021-06-03 10:03:31 -0600

    6-3-21

---
+++ js/dom-warmup.js
@@ -0,0 +1,27 @@
+"use strict";
+// function addClick() {
+//     document.getElementById("search")
+//         .addEventListener("click", addToDiv);
+// }
+// addClick()
+//
+// function addToDiv() {
+//     document.getElementById("post-container")
+//         .appendChild(getText());
+// }
+// function getText(){
+//     document.getElementById("input").value
+// }
+$(document).ready(function(){
+    console.log('document is ready')
+    $("#search").click(function(e){
+        e.preventDefault()
+        // console.log('click')
+        let str = $("#input").val()
+        let para = document.createElement("p")
+        para.innerText = str
+        // console.log(para)
+        document.getElementById("post-container")
+            .appendChild(para)
+    });
+});
